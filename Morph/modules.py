abb2b5ab15afdf375e555b2fa3ba261ac3157b1a
Andre Targino <andretargino@3071-AD-05075.local>
2025-02-14 21:17:45 -0600

    Add naïve counter

--- Morph/modules.py
+++ Morph/modules.py
@@ -1,3 +1,6 @@
+import numpy
+
+
 class Mapper:
     def naive(self, data):
         return data
@@ -15,3 +18,19 @@ class Mapper:
 
     def custom(self, data, mapper, *args):
         return mapper(data, *args)
+
+
+class Counter:
+    def naive(self, data, G):
+        image = {}
+        x = max(datum['x'] for datum in data) + 1
+        y = max(datum['y'] for datum in data) + 1
+        shape = (x, y)
+        for datum in data:
+            g = datum['g']
+            if g in G and g not in image:
+                image[g] = numpy.zeros(shape)
+                x = datum['x']
+                y = datum['y']
+                image[g][x, y] = 1
+        return image
