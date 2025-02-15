8131bfaeb69d408dd720c280c1a1028d232ec4ce
Andre Targino <andretargino@3071-AD-05075.local>
2025-02-14 18:21:26 -0600

    Add Visium mapper

--- Morph/modules.py
+++ Morph/modules.py
@@ -1,3 +1,8 @@
 class Mapper:
     def naive(self, data):
         return data
+
+    def visium(self, data):
+        for datum in data:
+            datum['x'] = (datum['x'] + datum['y']) // 2
+        return data
